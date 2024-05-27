6f11cdca3b0cc0bd69ffdb4a7f016a900d61625e
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-05-27 13:29:36 +0900

    [Silver III] Title: N과 M (1), Time: 312 ms, Memory: 30852 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N, M;
+    static int[] arr = new int[9];
+    static boolean[] visited = new boolean[9];
+
+    public static void main(String[] args) throws IOException {
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        sequence(0);
+
+        br.close();
+        bw.close();
+    }
+
+    static void sequence(int k) throws IOException {
+        if (k == M) {
+            for (int i = 0; i < M; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                arr[k] = i;
+                visited[i] = true;
+                sequence(k + 1);
+                visited[i] = false;
+            }
+        }
+     }
+}
