3806925e45f75a857cd6f733c1a3e29d17cf862e
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-06-24 19:36:55 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 112 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+	
+    public static void main(String[] args) throws IOException {
+    	int N = Integer.parseInt(br.readLine());
+    	
+    	for (int i = 1; i <= N; i++) {
+    		for (int j = N; j >= 1; j--) {
+    			if (j <= i) {
+    				bw.write("*");
+    			} else {
+    				bw.write(" ");
+    			}
+    		}
+    		bw.write("\n");
+    	}
+    	
+    	br.close();
+        bw.close();
+    }
+}
