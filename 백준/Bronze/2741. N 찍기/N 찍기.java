a7185ffffde706387f8999035beeedd765066e6d
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-06-30 12:23:36 +0900

    [Bronze V] Title: N 찍기, Time: 288 ms, Memory: 20856 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            bw.write(i + "\n");
+        }
+        
+        br.close();
+        bw.close();
+    }
+}
