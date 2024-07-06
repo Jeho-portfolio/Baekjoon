b736c9e431530fd1d3fe79e0e3af954b5c52e78d
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-07-06 11:17:53 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int score = Integer.parseInt(br.readLine());
+
+        if (score >= 90) {
+            bw.write("A");
+        } else if (score >= 80) {
+            bw.write("B");
+        } else if (score >= 70) {
+            bw.write("C");
+        } else if (score >= 60) {
+            bw.write("D");
+        } else {
+            bw.write("F");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
