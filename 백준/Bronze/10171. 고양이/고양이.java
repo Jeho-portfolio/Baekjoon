c234a6916c374e03085df24f20dcf12e33ba6cd6
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-07-07 20:06:32 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String args[]) throws Exception	{
+		
+		bw.write("\\    /\\\n");
+		bw.write(" )  ( ')\n");
+		bw.write("(  /  )\n");
+		bw.write(" \\(__)|\n");
+		
+		br.close();
+		bw.close();
+	}
+}
