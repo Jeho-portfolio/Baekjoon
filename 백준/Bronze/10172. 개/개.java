e49d136bdc83fe1e83005ea74b4fa6fd88c92890
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-07-08 10:34:45 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+
+        bw.write("|\\_/|\n");
+        bw.write("|q p|   /}\n");
+        bw.write("( 0 )\"\"\"\\\n");
+        bw.write("|\"^\"`    |\n");
+        bw.write("||_/=\\\\__|");
+
+        br.close();
+        bw.close();
+    }
+}
