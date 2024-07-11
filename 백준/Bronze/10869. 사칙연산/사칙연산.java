0ec9a580f922af9f7f5ead200ecac1685c7c85cd
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-07-11 19:07:11 +0900

    [Bronze V] Title: 사칙연산, Time: 148 ms, Memory: 16600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String args[]) throws Exception	{
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		bw.write((A + B) + "\n" + (A - B) + "\n" + (A * B) + "\n" + (A / B) + "\n" + (A % B));
+		
+		br.close();
+		bw.close();
+	}
+}
