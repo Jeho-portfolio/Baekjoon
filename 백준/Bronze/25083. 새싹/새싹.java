2dcac5e99723ef404a14c88b0048b0dd896a4b25
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-07-17 17:21:34 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String args[]) throws Exception	{
+		bw.write("         ,r'\"7\n");
+		bw.write("r`-_   ,'  ,/\n"); 
+		bw.write(" \\. \". L_r'\n");
+		bw.write("   `~\\/\n");
+		bw.write("      |\n");
+		bw.write("      |");
+		
+		br.close();
+		bw.close();
+	}
+}
