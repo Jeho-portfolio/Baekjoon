d0332e80b64b8004283ccf7c8f495925da552db8
Jeho Jeong <121224745+Jeho-portfolio@users.noreply.github.com>
2024-09-16 12:47:20 +0900

    [Unrated] Title: 휴식이 필요해, Time: 0 ms, Memory: 0 KB -BaekjoonHub

---
+++ 백준/Unrated/16076. 휴식이 필요해/휴식이 필요해.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String args[]) throws Exception	{
+        bw.write("JAVA");
+
+        br.close();
+        bw.close();
+    }
+}
